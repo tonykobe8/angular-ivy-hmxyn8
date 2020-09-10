6f1f114c81f398cdd47c0d08d721a4296b640dfc
tonykobe8 <tonykobe8@gmail.com>
2020-09-10 08:34:24 +0200

    1 file added

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmployeeComponent } from './employee/employee.component';
+import { ProjectComponent } from './project/project.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
